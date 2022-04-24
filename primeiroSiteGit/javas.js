89476ec9890f326b89f9488e34b1487e85252f7a
Zilmar Junio Andrade Vieira <zilmarjjunio@gmail.com>
2022-04-23 21:24:20 -0300

    coloquei um arq .js

---
+++ primeiroSiteGit/javas.js
@@ -0,0 +1,22 @@
+//Script da pag Ola-Mundo
+
+let meuNome = 'ILMAR'
+
+console.log(`Olá ${meuNome}`)
+
+
+if(meuNome == 'Zilmar'){
+
+    meuNome = meuNome.toUpperCase()
+    console.log(meuNome)
+
+}else{
+
+    meuNome = meuNome.toLowerCase()
+    console.log(meuNome)
+
+}
+
+
+
+//alert(`Olá ${meuNome}`)
