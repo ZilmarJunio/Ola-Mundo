d4776eb42d6c7d248110001ab182d0666e783f39
Zilmar Junio Andrade Vieira <zilmarjjunio@gmail.com>
2022-04-24 19:51:16 -0300

    fiz a fizzbuzz do meu jeito
    ficou meio mee

--- primeiroSiteGit/javas.js
+++ primeiroSiteGit/javas.js
@@ -1,6 +1,6 @@
 //Script da pag Ola-Mundo
 
-let meuNome = 'ILMAR'
+/*let meuNome = 'Zilmar'
 
 console.log(`Olá ${meuNome}`)
 
@@ -15,8 +15,60 @@ if(meuNome == 'Zilmar'){
     meuNome = meuNome.toLowerCase()
     console.log(meuNome)
 
+}*/
+
+//fizzbuzz ->
+
+{
+
+console.log(fizzBuzz(15))
+
+function fizzBuzz(numero){
+
+    let numeroFun = numero
+    if(typeof numeroFun !== 'number'){
+
+        return 'Não é um número'
+
+    }
+
+    if(numeroFun%3 === 0 && numeroFun%5 === 0){
+        numeroFun = 'divisivelPor3e5'
+    }else{
+
+        numeroFun = numeroFun%3 === 0 ? 'divisivelPor3' : numeroFun = numeroFun%5 === 0 ? 'divisivelPor5' : numero
+        
+    }
+
+    switch(numeroFun){
+
+        case 'divisivelPor3':
+
+        return 'Fizz'
+
+        break
+
+        case 'divisivelPor5':
+
+        return 'Buzz'
+
+        break
+
+        case 'divisivelPor3e5':
+
+           return 'FizzBuzz'
+        
+        break
+
+        default: 
+
+        return numeroFun
+
+        break
+
+    }
+    
 }
-
-
+}
 
 //alert(`Olá ${meuNome}`)
