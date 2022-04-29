ce89d62fbe20b4708909cf6fd3968ab575be7b69
Zilmar Junio Andrade Vieira <zilmarjjunio@gmail.com>
2022-04-28 23:55:38 -0300

    mudei mais algumas coisas

--- 1. Cronômetro/javas.js
+++ 1. Cronômetro/javas.js
@@ -1,6 +1,20 @@
 //Cronometro
 
-let hora = 0, minuto = 0, segundo = 0;
 let horas = document.getElementById('hora');
 let minutos = document.getElementById('minuto');
 let segundos = document.getElementById('segundo');
+
+let hora = 0, minuto = 0, segundo = 0;
+
+function Start(){
+
+  setInterval(Somar, 1000);
+
+} 
+
+function Somar(){
+
+  segundo++
+  segundos.innerHTML = `${segundo}`
+
+}
