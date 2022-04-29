f54eb83fecf2b29096c528b715e78e267912fdee
Zilmar Junio Andrade Vieira <zilmarjjunio@gmail.com>
2022-04-28 22:51:50 -0300

    recomecei tudo

--- 1. Cronômetro/teste.js
+++ 1. Cronômetro/teste.js
@@ -1,10 +1,6 @@
-setTimeout(() => {
-                
-    /*if(horas <= 10){ horas = '0' + horas }
-    if(minutos <= 10){ minutos = '0' + minutos }*/
-    if(segundos <= 10){ segundos = '0' + segundos }
 
-    if(minutos<10){ Count.innerHTML = `00:0${minutos}:${segundos}`}
+setInterval(() => {
     
+    console.log('oi')
 
-}, segundos*1000);
+}, 1000);
