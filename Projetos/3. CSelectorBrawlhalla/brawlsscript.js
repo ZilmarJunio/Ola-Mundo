6d5a54f0ae4bccb08a808c9c2157cfb5db2e89ae
Zilmar Junio Andrade Vieira <zilmarjjunio@gmail.com>
2022-05-17 22:21:21 -0300

    arrumei o bug do timer

--- Projetos/3. CSelectorBrawlhalla/brawlsscript.js
+++ Projetos/3. CSelectorBrawlhalla/brawlsscript.js
@@ -3,7 +3,7 @@ let Legend_2 = document.getElementById('secondc');
 let Legend_3 = document.getElementById('thirdc');
 
 let Interval, Interval2, Interval3;
-let count2 = 0, count3 = 0, count1 = 0;
+let count2 = 0, count3 = -1, count1 = 0;
 
 let brawlhalla = [
     'Bödvar', 'Cassidy','Orion','Lord Vraxx','Gnash','Queen Nai','Hattori','SirRoland',
@@ -16,13 +16,11 @@ let brawlhalla = [
 
 function runInterval(){
 
-    count2 = 20, count3 = 30, count1 = 10;
-
     Interval = setInterval(() => {
        
         let randomL = brawlhalla[Math.floor(Math.random() * brawlhalla.length)]
         Legend_1.innerHTML = `${randomL}`
-        if(count1 == 0){
+        if(count1 === 0){
             fontColorb(1)
             clearInterval(Interval);
             }
@@ -35,7 +33,7 @@ function runInterval(){
        
         let randomL = brawlhalla[Math.floor(Math.random() * brawlhalla.length)]
         Legend_2.innerHTML = `${randomL}`
-        if(count2 == 0){
+        if(count2 === 0){
             fontColorb(2)
             clearInterval(Interval2);
             }
@@ -48,27 +46,41 @@ function runInterval(){
        
         let randomL = brawlhalla[Math.floor(Math.random() * brawlhalla.length)]
         Legend_3.innerHTML = `${randomL}`
-        if(count3 == 0){
+        if(count3 === 0){
             fontColorb(3)
-            clearInterval(Interval3);
+            clearInterval(Interval3)
             }
         
-        if(count3 > 0){count3--;}
+        count3--;
+        console.log(count3);
 
     }, 100);
+
+    
 
 }
 
 function Reset() {
     
-    if (count3 == 0) {
+    if (count3 === -1) {
+
+        resetValues()
         fontColory()
         runInterval()
+        
     }
 
   }
 
-  function fontColory() {
+function resetValues() {
+
+    count1 = 10; 
+    count2 = 20;
+    count3 = 30;
+    
+}
+
+function fontColory() {
       
     Legend_1.style.color = "rgb(0, 0, 0)";
     Legend_2.style.color = "rgb(0, 0, 0)";
@@ -76,7 +88,7 @@ function Reset() {
 
   }
 
-  function fontColorb(value) {
+function fontColorb(value) {
 
     switch (value) {
         case 1:
